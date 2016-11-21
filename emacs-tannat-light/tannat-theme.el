b8de2202c70d689636c7755e159cb4d5ed8cdb66
Matthew Lyon <matthew@lyonheart.us>
2016-11-20 23:06:04 -0800

    Initial Emacs Theme

---
+++ emacs-tannat-light/tannat-theme.el
@@ -0,0 +1,129 @@
+(deftheme tannat
+  "A UI Theme for Emacs")
+(display-color-cells (selected-frame))
+(let* ((class '((class color) (min-colors 89)))
+
+       (base0 "#f6f3f2")
+       (base1 "#ece9e4")
+       (base2 "#d5d7cc")
+       (base3 "#586e68")
+       (base4 "#445967")
+       (base5 "#2f3d58")
+
+       (yel0 "#f6d367")
+       (yel1 "#825b0e")
+       (yel2 "#644111")
+       (red0 "#f6ccd3")
+       (red1 "#c21b1b")
+       (red2 "#89230e")
+       (pur0 "#f1caf1")
+       (pur1 "#b31398")
+       (pur2 "#8e005e")
+       (blu0 "#aadfe9")
+       (blu1 "#106895")
+       (blu2 "#00478f")
+       (cya0 "#99e5cc")
+       (cya1 "#0c6f5f")
+       (cya2 "#005151")
+       (grn0 "#c2e418")
+       (grn1 "#426e05")
+       (grn2 "#1c5400")
+       )
+  (custom-theme-set-faces
+   'tannat
+   `(default ((,class (:foreground ,base5 :background ,base1))))
+   `(bold ((,class (:weight bold))))
+   `(bold-italic ((,class (:weight bold :slant italic))))
+   `(underline ((,class (:underline t))))
+   `(italic ((,class (:slant italic))))
+
+   `(font-lock-builtin-face ((,class (:foreground ,base5 :background ,blu0))))
+   `(font-lock-comment-delimiter-face ((,class (:foreground ,base3 :slant italic))))
+   `(font-lock-comment-face ((,class (:foreground ,base3 :slant italic))))
+   `(font-lock-constant-face ((,class (:foreground ,base5 :background ,blu0))))
+   `(font-lock-doc-face ((,class (:foreground ,base5))))
+   `(font-lock-doc-string-face ((,class (:foreground ,base5))))
+   `(font-lock-function-name-face ((,class (:foreground ,base5 :weight bold))))
+   `(font-lock-keyword-face ((,class (:foreground ,base5 :background ,pur0))))
+   `(font-lock-negation-char-face ((,class (:foreground ,base5))))
+   `(font-lock-preprocessor-face ((,class (:foreground ,base5))))
+   `(font-lock-regexp-grouping-backslash ((,class (:foreground ,base5 :background ,grn0))))
+   `(font-lock-regexp-grouping-construct ((,class (:foreground ,base5 :background ,grn0))))
+   `(font-lock-string-face ((,class (:foreground ,base5 :background ,grn0))))
+   `(font-lock-type-face ((,class (:foreground ,base5))))
+   `(font-lock-variable-name-face ((,class (:foreground ,base5))))
+   `(font-lock-warning-face ((,class (:weight bold :foreground ,base5))))
+   `(highlight-numbers-number ((,class (:foreground ,base5 :background ,cya0))))
+   `(shadow ((,class (:foreground ,base5))))
+   `(success ((,class (:foreground ,base5))))
+   `(error ((,class (:foreground ,base5))))
+   `(warning ((,class (:foreground ,base5))))
+
+   ;; Emacs
+   `(border ((,class (:background ,base1))))
+   `(button ((,class (:foreground ,base5 :underline nil))))
+   `(vertical-border ((,class (:background ,base1 :foreground ,base2))))
+   `(fringe ((,class (:background ,base1))))
+   `(highlight ((,class (:background nil :underline t))))
+   `(hl-line ((,class (:background nil :underline t))))
+
+   ;; ahs
+   `(ahs-definition-face ((,class (:background ,base1 :foreground ,base5))))
+   `(ahs-edit-mode-face ((,class (:background ,base1 :foreground ,base5))))
+   `(ahs-face ((,class (:background ,base1 :foreground ,base5))))
+   `(ahs-plugin-bod-face ((,class (:background ,base1 :foreground ,base5))))
+   `(ahs-plugin-defalt-face ((,class (:background ,base1 :foreground ,base5))))
+   `(ahs-plugin-whole-buffer-face ((,class (:background ,base1 :foreground ,base5))))
+   `(ahs-warning-face ((,class (:background ,base1 :foreground ,base5))))
+
+   ;; anzu
+   `(anzu-match-1 ((,class (:background ,base1 :foreground ,base5))))
+   `(anzu-match-2 ((,class (:background ,base1 :foreground ,base5))))
+   `(anzu-match-3 ((,class (:background ,base1 :foreground ,base5))))
+   `(anzu-mode-line ((,class (:background ,base1 :foreground ,base5))))
+   `(anzu-mode-line-no-match ((,class (:background ,base1 :foreground ,base5))))
+   `(anzu-replace-highlight ((,class (:background ,base1 :foreground ,base5))))
+   `(anzu-replace-to ((,class (:background ,base1 :foreground ,base5))))
+
+   ;; company
+   `(company-preview ((,class (:background ,base1))))
+   `(company-preview-common ((,class (:background ,base1))))
+   `(company-preview-search ((,class (:background ,base1))))
+   `(company-scrollbar-bg ((,class (:background ,base1))))
+   `(company-scrollbar-fg ((,class (:background ,base1))))
+   `(company-template-field ((,class (:background ,base1))))
+   `(company-tooltip ((,class (:background ,base1 :foreground ,base5))))
+   `(company-tooltip-annotation ((,class (:foreground ,base5))))
+   `(company-tooltip-annotation-selection ((,class (:foreground ,base5))))
+   `(company-tooltip-common-selection ((,class (:background ,base0))))
+   `(company-tooltip-selection ((,class (:background ,base0))))
+
+   ;; Modeline
+   `(mode-line ((,class (:background ,base0))))
+   `(mode-line-highlight ((,class (:box nil))))
+   `(mode-line-inactive ((,class (:background ,base5 :foreground ,base1))))
+   ;; Powerline
+   `(powerline-active1 ((,class (:background ,base2))))
+   `(powerline-active2 ((,class (:background ,yel0))))
+   `(powerline-inactive1 ((,class (:background ,base4 :foreground ,base2))))
+   `(powerline-inactive2 ((,class (:background ,base3 :foreground ,base2))))
+   ;; Spaceline
+   `(spaceline-evil-normal ((,class (:background ,yel0))))
+   `(spaceline-evil-insert ((,class (:background ,grn0))))
+   `(spaceline-evil-visual ((,class (:background ,cya0))))
+
+   ;; Spacemacs
+   `(spacemacs-normal-face ((,class (:background ,yel0))))
+   `(spacemacs-insert-face ((,class (:background ,grn0))))
+   `(spacemacs-visual-face ((,class (:background ,base0))))
+
+   ;; Which-Key
+   `(which-key-group-description-face ((,class (:background ,base1 :foreground ,pur2))))
+   `(which-key-key-face ((,class (:background ,base1 :foreground ,blu1 :weight bold))))
+   ))
+
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'tannat)
